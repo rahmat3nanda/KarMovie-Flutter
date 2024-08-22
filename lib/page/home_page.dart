0cc73700d0c0212b91014485aa7c1bd1c760565b
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-23 05:15:37 +0700

    create home page

---
+++ lib/page/home_page.dart
@@ -0,0 +1,23 @@
+/*
+ * *
+ *  * home_page.dart - KarMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 08/23/2024, 05:15
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 08/23/2024, 05:15
+ *
+ */
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
