34c4a195f11c5db1f4b849aa4df63bb277734711
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-23 05:27:50 +0700

    create api

---
+++ lib/data/API.dart
@@ -0,0 +1,16 @@
+/*
+ * *
+ *  * API.dart - KarMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 08/23/2024, 05:27
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 08/23/2024, 05:27
+ *
+ */
+
+class API {
+  APIFilm genre = APIFilm();
+}
+
+class APIFilm {
+  final String data = "films";
+}
