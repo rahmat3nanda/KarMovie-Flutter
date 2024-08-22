712d9b4576594abc0758f604b286d4e39fd472cf
Rahmat Trinanda <rahmat3nanda@gmail.com>
2024-08-23 05:29:36 +0700

    create request

---
+++ lib/data/request.dart
@@ -0,0 +1,28 @@
+/*
+ * *
+ *  * request.dart - KarMovie
+ *  * Created by Rahmat Trinanda (rahmat3nanda@gmail.com) on 08/23/2024, 05:28
+ *  * Copyright (c) 2024 . All rights reserved.
+ *  * Last modified 08/23/2024, 05:28
+ *
+ */
+
+import 'package:dio/dio.dart' as dio;
+import 'package:kar_movie/data/repo.dart';
+
+late Repo _repo;
+
+class Request {
+  late RequestFilm film;
+
+  Request() {
+    _repo = Repo();
+    film = RequestFilm();
+  }
+}
+
+class RequestFilm {
+  Future<dio.Response> data() {
+    return _repo.film.data();
+  }
+}
